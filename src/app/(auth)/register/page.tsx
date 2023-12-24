2b62afea76ebae694fbc09c60dafd98b959f64f1
Kabir Bose <kabirbose04@gmail.com>
2023-12-24 16:42:02 -0500

    created register form and page

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "@/components/auth/RegisterForm";
+
+export default function Register() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <RegisterForm />
+    </div>
+  );
+}
---
+++ src/components/auth/RegisterForm.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useState } from "react";
+
+export default function RegisterForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      setError("Please fill all fields");
+      return;
+    }
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex justify-center items-center gap-3 flex-col"
+      >
+        <input
+          className="text-black"
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Username"
+        />
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          className="text-black"
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          className="text-black"
+          type="password"
+          placeholder="Password"
+        />
+        <button type="submit">Register</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
